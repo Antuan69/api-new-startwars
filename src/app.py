82856dbbf3d2bb2939d54b48e1c9c0da7721d34e
Antonio Gomez <totico388@hotmail.com>
2025-01-24 20:32:58 +0000

    Sin terminar

--- src/app.py
+++ src/app.py
@@ -8,8 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, Personajes, Planetas
+from sqlalchemy import select 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -36,15 +36,83 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+#obtener todos los personajes 
+@app.route('/personajes', methods=['GET'])
+def todos_los_personajes():
+
+    data = db.session.scalars(select(Personajes)).all()
+    results = list(map(lambda Personajes: Personajes.serialize(),data))
+    
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Hello, this is your GET /user response ", 
+        "results":results
     }
 
     return jsonify(response_body), 200
 
+# obtener un solo personaje
+@app.route('/personajes/<int:id>', methods=['GET'])
+def solo_un_personaje(id):
+    
+ try: 
+
+    personaje = db.session.execute(select(Personajes).filter_by(id=id)).scalar_one()
+    
+    
+    
+
+    response_body = {
+        "msg": "Hello, this is your GET /personaje response ", 
+        "results":personaje.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+ except: 
+
+    return jsonify({"msg":"user not exist"}), 404
+ 
+
+#obtener todos los planetas
+@app.route('/planetas', methods=['GET'])
+def todos_los_planetas():
+
+    data = db.session.scalars(select(Planetas)).all()
+    results = list(map(lambda Planetas: Planetas.serialize(),data))
+    
+
+    response_body = {
+        "msg": "Hello, this is your GET /todos los planetas response ", 
+        "results":results
+    }
+
+    return jsonify(response_body), 200 
+
+
+# obtener un solo planeta
+@app.route('/planetas/<int:id>', methods=['GET'])
+def solo_un_planeta(id):
+    
+ try: 
+
+    planeta = db.session.execute(select(Planetas).filter_by(id=id)).scalar_one()
+    
+    
+    
+
+    response_body = {
+        "msg": "Hello, this is your GET /planetaresponse ", 
+        "results":planeta.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+ except: 
+
+    return jsonify({"msg":"planeta not exist"}), 404
+
+ 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import mapped_column
-from sqlalchemy import Integer, String, Boolean
+from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy import Integer, String, Boolean, ForeignKey 
 
 db = SQLAlchemy()
 
@@ -12,6 +12,8 @@ class User(db.Model):
     email = mapped_column(String(120), nullable=False)
     password = mapped_column(String(80))
     is_active = mapped_column(Boolean)
+    # aqui estoy accediendo desde el objeto favoritos puedo 
+    favoritos = relationship("Favoritos", back_populates="user") 
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -22,6 +24,59 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+class Personajes(db.Model):
+    __tablename__ = "personaje"
+
+    id = mapped_column(Integer, primary_key=True)
+    altura = mapped_column(String(120), nullable=False)
+    color_de_pelo = mapped_column(String(80))
+    color_de_ojos = mapped_column(String(80))
+    genero =  mapped_column(String(80))   
+    favoritos = relationship("Favoritos", back_populates ="personaje")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "altura": self.altura,
+            "color_de_pelo": self.color_de_pelo,
+            "color_de_ojos": self.color_de_ojos,
+            "genero": self.genero,
+        }
+
+class Planetas(db.Model):
+    __tablename__ = "planetas"
+
+    id = mapped_column(Integer, primary_key=True)
+    poblacion = mapped_column(String(120), nullable=False)
+    terreno = mapped_column(String(80), nullable = False)
+    admosfera = mapped_column(String(80), nullable = False)
+    favoritos = relationship("Favoritos", back_populates = "planeta")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "poblacion": self.poblacion,
+            "terreno": self.terreno,
+            "admosfera": self.admosfera
+            
+        }
+
+class Favoritos(db.Model):
+    __tablename__ = "favoritos"
+
+    id = mapped_column(Integer, primary_key=True) 
+
+    planeta_id = mapped_column(Integer, ForeignKey("planetas.id"), nullable=True) 
+    planeta =  relationship ("Planetas", back_populates = "favoritos")
+
+    personajes_id = mapped_column(Integer, ForeignKey("personaje.id"), nullable=True)
+    personaje = relationship ("Personajes", back_populates = "favoritos")
+
+    user_id = mapped_column(Integer, ForeignKey("user.id"), nullable = False)
+    user = relationship("User", back_populates = "favoritos")
+
+    
 
 
 
